6c1d9b4750be54490bb8565b3c2794c874fe2b63
TypcalSolution <125317872+TypcalSolution@users.noreply.github.com>
2024-01-30 15:55:21 -0300

    Add files via upload

--- Ariel.py
+++ Ariel.py
@@ -1,71 +1,181 @@
-import PySimpleGUI as sg
-import pyautogui as pa
 from pytube import YouTube
 from youtube_search import YoutubeSearch
 import os
+from pyigdl import IGDownloader
 
-sg.theme('Dark Blue')
-layout = [
-    [sg.Text("ARIEL", size=(0, 2), font=('arial', 33), pad=(195, 0))],
-    [sg.Input(pad=(23, 0), size=(30, 1), font=('arial', 20))],
-    [sg.Button('Add', size=(7, 1), pad=(80, 2), font=('hervitica', 20), button_color='#6bd15b'),
-     sg.Button('Remove', pad=(30, 0), size=(7, 1), font=('hervitica', 20), button_color='#E93D3D')],
-    [sg.Button('Show list', size=(10, 1), pad=(170, 5), font=('hervitica', 20), button_color='#3D96E9')],
-    [sg.Button('Download', size=(10, 1), pad=(170, 5), font=('hervitica', 20), button_color='white')],
-]
 
-window = sg.Window('Ariel', layout, icon='ARIEL.ico')
 LIST_MUSICS = []
+local = fr'C:\Users\{os.getlogin()}\Downloads'
+def HEAD():
+    os.system('cls')
+    print('''
+         _      ____    ___   _____   _     
+        / \    |  _ \  |_ _| | ____| | |    
+       / _ \   | |_) |  | |  |  _|   | |    
+      / ___ \  |  _ <   | |  | |___  | |___ 
+     /_/   \_\ |_| \_\ |___| |_____| |_____|
+    ''')
+    inicio()
 
-while True:
-    event, values = window.read()
-    print(event, values)
 
-    if event == 'Download':
-        results = YoutubeSearch(LIST_MUSICS[0], max_results=1).to_dict()
-        titulo = results[0]['title']
-        author = results[0]['channel']
-        duration = results[0]['duration']
-        url = 'youtube.com' + results[0]['url_suffix']
 
-    #CONFIRMATION=======================
-        l = [
-            [sg.Text(text='Do you wanna Continue?', size=(20,2), font=('arial', 30))],
-            [sg.Button('Yes', size=(6,1), pad=(90, 2), font=('hervitica', 15), button_color='#6bd15b'),
-             sg.Button('No', size=(6,1), pad=(35, 0),font=('hervitica', 15), button_color='#E93D3D')],
-        ]
-        win = sg.Window('Confirmation', l, icon='ARIEL.ico')
-        while True:
-            event, values = win.read()
-            if event == 'No':
-                quit()
 
-            elif event == 'Yes':
-                for music in LIST_MUSICS:
-                    results = YoutubeSearch(music, max_results=1).to_dict()
+def music_downloader():
+    print('''
+    -- Módulo de download de músicas --
+    Selecione a opção desejada:
+    [1] Para realizar o download
+    [2] Para começar a adicionar as músicas
+    [3] Para voltar ao menu principal
+    [4] Para Mostrar músicas que seram baixadas
+    [5] Para limpar a lista de músicas INTEIRA
+    ''')
+
+    i = int(input('Opção Desejada: '))
+
+    if i == 1:
+        if LIST_MUSICS:
+            for music in LIST_MUSICS:
+                if 'https://' in music:
+                    try:
+                        video = YouTube(music,use_oauth=True, allow_oauth_cache=False)
+                        stream = video.streams.filter(only_audio=True, abr='320kbps').first()
+                        stream.download(local,
+                                        filename=f"{video.title}.mp3")  # caminho do diretorio para download
+                        print(f"A musica {video.title} foi baixado")
+                        inicio()
+                    except:
+                        print(f'A musica {video.title} não foi baixada.')
+                        inicio()
+
+            else:
+                results = YoutubeSearch(music, max_results=1).to_dict()
+                try:
                     url = 'youtube.com' + results[0]['url_suffix']
-                    video = YouTube(url)
-                    stream = video.streams.filter(only_audio=True).first()
-                    stream.download(fr'C:\Users\{os.getlogin()}\Downloads',filename=f"{video.title}.mp3")
-                    #my_video.download('~/Downloads')
-                    pa.alert(text=f"A musica {video.title} foi baixado")
-                    quit()
+                    video = YouTube(url,use_oauth=True, allow_oauth_cache=False)
+                    stream = video.streams.filter(only_audio=True, abr='320kbps').first()
+                    stream.download(local,
+                                    filename=f"{video.title}.mp3")  # caminho do diretorio para download
+                    print(f"A musica {video.title} foi baixado")
+                    inicio()
 
-    elif event == 'Add':
-        nome = values[0]
-        LIST_MUSICS.append(nome)
-        print(LIST_MUSICS)
+                except:
+                    print(f'A musica {video.title} não foi baixada.')
+                    inicio()
+        else:
+            print('A lista de músicas está vazia!')
+            music_downloader()
 
-    elif event == 'Remove':
-        nome = values[0]
-        LIST_MUSICS.remove(nome)
-        print(LIST_MUSICS)
+    elif i == 2:
+        while True:
+            nome = input('Nome da música/Link do youtube/Opção Desejada: ')
 
-    elif event == 'Show list':
-        pa.alert(text=LIST_MUSICS, title='Ariel')
-        print(LIST_MUSICS)
+            if nome == '1':
+                for music in LIST_MUSICS:
+                    if 'https://' in music:
+                        try:
+                            video = YouTube(music)
+                            stream = video.streams.filter(only_audio=True).first()
+                            stream.download(fr'C:\Users\{os.getlogin()}\Downloads', filename=f"{video.title}.mp3")  # caminho do diretorio para download
+                            print(f"A musica {video.title} foi baixado")
+                            inicio()
+                        except Exception as e:
+                            print(e)
+                            print(f'A musica {music} não foi baixada.')
+                            inicio()
+
+                    else:
+                        results = YoutubeSearch(music, max_results=1).to_dict()
+                        try:
+                            url = 'youtube.com' + results[0]['url_suffix']
+                            video = YouTube(url)
+                            stream = video.streams.filter(only_audio=True).first()
+                            stream.download(fr'C:\Users\{os.getlogin()}\Downloads', filename=f"{video.title}.mp3")  # caminho do diretorio para download
+                            print(f"A musica {video.title} foi baixado")
+                            inicio()
+
+                        except Exception as e:
+                            print(e)
+                            print(f'A musica {music} não foi baixada.')
+                            inicio()
+                break
+
+            elif nome == '3':
+                HEAD()
+
+            elif nome == '4':
+                if LIST_MUSICS:
+                    for musica in LIST_MUSICS:
+                        print(musica)
+                else:
+                    print('A lista de músicas está vazia')
 
 
-    elif event == None:
-        break
+            elif nome == '5':
+                if LIST_MUSICS:
+                    LIST_MUSICS.clear()
+                    print('A lista de músicas está vazia')
+                    music_downloader()
+                else:
+                    print('A lista já está vazia')
+                    music_downloader()
+
+            else:
+                LIST_MUSICS.append(nome)
+    elif i == 3:
+        HEAD()
+
+    elif i == 4:
+        if LIST_MUSICS:
+            for musica in LIST_MUSICS:
+                print(musica)
+        else:
+            print('A lista de músicas está vazia')
+            music_downloader()
+
+    elif i == 5:
+        if LIST_MUSICS:
+            LIST_MUSICS.clear()
+            print('A lista de músicas está vazia')
+            music_downloader()
+        else:
+            print('A lista já está vazia')
+            music_downloader()
+
+def reel_download():
+    while True:
+        print('Digite "1" para voltar ao menu principal')
+        data = input('Link do Reel/Opção Desejada: ')
+        print('\n')
+        if data == '1':
+            HEAD()
+        else:
+            data = IGDownloader(data)
+            link = data[0]["download_link"]
+            os.system(f'start {link}')
+
+
+def inicio():
+    print('''
+    Selecione a opção desejada:
+    [1] Onde será salvo os arquivos
+    [2] Baixador de músicas
+    [3] Baixador de Reels do Instagram
+    \n''')
+    i = int(input('Opção Desejada: '))
+    if i == 1:
+        print('Local de Download padrão é "Downloads"')
+        local = input('Indique o novo local de Download: ')
+        print(f'Novo local de Download: {local}')
+        inicio()
+
+    elif i == 2:
+        music_downloader()
+
+    elif i == 3:
+        reel_download()
+
+HEAD()
+
+
 
